ecf866d47831251563435811f92377b70faf4c28
Ian Altemus <YOUR_EMAIL_HERE>
2026-03-07 08:56:34 -0800

    Feat: VPC deployed to AWS - subnets,IGW,NAT,rts,sgs

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" { 
-        bucket = "pulse-terraform-state-dev-894943009636"
+        bucket = "pulse-tf-state-dev-894943009636"
         key = "dev/terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "pulse-terraform-locks-dev"
